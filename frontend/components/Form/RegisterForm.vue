00b541c37b8931af967d4a6f435ee009961f8df7
Samuel Dhondt <samueldhondt@MacBook-Air-de-Samuel.local>
2024-05-02 11:43:59 +0200

    add rules to form

--- frontend/components/Form/RegisterForm.vue
+++ frontend/components/Form/RegisterForm.vue
@@ -9,7 +9,7 @@
       el-input(v-model="registerPayload.password" type="password" show-password)
     el-form-item(label="confirmpassword" prop="checkPass" autocomplete="off")
       el-input(v-model="registerPayload.confirmpassword" type="password" show-password)
-    el-form-item(label="Role" prop="role")
+    el-form-item(label="Role" prop="role_id")
       el-select(v-model='registerPayload.role_id')
         el-option(
           v-for="(item, i) in role_choice"
@@ -33,33 +33,32 @@ export default {
   name: "RegistrationForm",
   data() {
     const validatePass = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('Enter a password'));
-      } else {
-        if (this.registerPayload.checkPass !== '') {
-          this.$refs.registerPayload.validateField('checkPass');
+        if (this.registerPayload.password === '') {
+          callback(new Error('Veuillez entrer le mot de passe'));
+        } else {
+          if (this.registerPayload.confirmpassword !== '') {
+            this.$refs.registerPayload.validateField('checkPass');
+          }
+          callback();
         }
-        callback();
-      }
-    }
-    const validatePass2 = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('Please re-enter the password'));
-      } else {
-        if (this.registerPayload.checkPass !== '') {
-          this.$refs.registerPayload.validateField('checkPass');
+      };
+      const validatePass2 = (rule, value, callback) => {
+        if (this.registerPayload.confirmpassword === '') {
+          callback(new Error('Veuillez entrer à nouveau le mot de passe'));
+        } else if (this.registerPayload.confirmpassword !== this.registerPayload.password) {
+          callback(new Error('Les deux entrées ne correspondent pas!'));
+        } else {
+          callback();
         }
-        callback();
-      }
     }
     return {
       registerPayload: Object.assign({}, defaultRegisterPayload),
       rules: {
         name: [{ required: true, message: "Username is required", trigger: "blur" }],
         email: [{ required: true, message: "Email is required", trigger: "blur" }],
-        pass: [{ validator: validatePass, trigger: 'blur' }],
-        checkPass: [{ validator: validatePass2, trigger: 'blur' }],
-        role: [{ required: true, message: "role is required", trigger: "blur" }]
+        pass: [{required: true, validator: validatePass, trigger: 'blur' }],
+        checkPass: [{required: true, validator: validatePass2, trigger: 'blur' }],
+        role_id: [{ required: true, message: "role is required", trigger: "blur" }]
       },
     };
   },
